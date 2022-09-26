2cbf3c9fd314ace94ecae3b06a536ce686af4dc0
Alexis Lust <alexis.lust@gmail.com>
2022-09-26 14:26:38 -0400

    initial api connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import Welcome from "./components/Welcome"
+import React, { useEffect, useState } from "react";
+import Users from "./components/User";
 
 
 export default function App() {
+    const [backendData, setBackendData] = useState([{}])
+
+    useEffect(() => {
+      fetch("/api").then(
+        response => response.json()
+      ).then(
+        data => {
+          setBackendData(data)
+        }
+      )
+    }, [])
+
     return (
       <>
-        <Welcome />
+      <div>
+        {(typeof backendData.users === 'undefined') ? (
+          <p>loading...</p>
+        ): (
+          backendData.users.map((user, i) => (
+            <p key={i}>{user}</p>
+          ))
+        )}
+      </div>
+      {/* <Users /> */}
+        {/* <Welcome /> */}
       </>
     );
   }
---
+++ src/components/User.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Users() {
+  
+    return(
+        <div>
+            <h1 className="text-black text-sm">Users Component</h1>
+        </div>
+    ) 
+  
+}
+
+
